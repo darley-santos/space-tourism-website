8afd3fc110c778a66f3ce860dae5b7017b8590aa
darley-santos <122935576+darley-santos@users.noreply.github.com>
2025-02-06 21:13:44 -0300

    Refatoração re código

--- javascript/index.js
+++ javascript/index.js
@@ -1,39 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll(".navbar-list li a, .planets-nav ul li a");
-    const currentPagePath = window.location.pathname;
-  
-    links.forEach(function(link) {
-      const linkPath = link.getAttribute("href");
-  
-      if (currentPagePath.includes(linkPath)) {
-        link.parentElement.classList.add("active");
-      }
-    });
-  });
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    const currentPathname = window.location.pathname; // Obtém o pathname da página atual
-    
-    const carouselLinks = document.querySelectorAll('.carousel-link');
-    
-    // Adiciona a classe "actived" ao link correspondente ao pathname atual
-    carouselLinks.forEach(link => {
-      if (link.getAttribute('href') === currentPathname) {
-        link.classList.add('actived');
-      }
-    });
-  });
+    function highlightActiveLinks(selector, activeClass) {
+        const links = document.querySelectorAll(selector);
+        const currentPath = window.location.pathname;
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const currentPathname = window.location.pathname; // Obtém o pathname da página atual
-    
-    const carouselLinks = document.querySelectorAll('.CarouselLink');
-    
-    // Adiciona a classe "actived" ao link correspondente ao pathname atual
-    carouselLinks.forEach(link => {
-      if (link.getAttribute('href') === currentPathname) {
-        link.classList.add('clicked');
-      }
-    });
-  });
-  
+        links.forEach(link => {
+            if (link.getAttribute("href") && currentPath.includes(link.getAttribute("href"))) {
+                link.classList.add(activeClass);
+            }
+        });
+    }
+
+    // Aplicar a lógica para diferentes seletores
+    highlightActiveLinks(".navbar-list li a, .planets-nav ul li a", "active");
+    highlightActiveLinks(".carousel-link", "actived");
+    highlightActiveLinks(".CarouselLink", "clicked");
+});
